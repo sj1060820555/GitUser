36e6388996d7bfb781c590e083f3b7c84e807c39
weibiao li <weibiaoli@weibiaodeimac.local>
2018-04-09 09:23:49 +0800

    Initial Commit

---
+++ SJGitUser/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  SJGitUser
+//
+//  Created by weibiao li on 2018/4/9.
+//  Copyright © 2018年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ SJGitUser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJGitUser
+//
+//  Created by weibiao li on 2018/4/9.
+//  Copyright © 2018年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
