512ffd82c55f573b2b75f3d37744677c8a403f8f
weibiao li <weibiaoli@weibiaodeiMac.local>
2018-04-09 11:25:17 +0800

    vc three

---
+++ SJGitUser/vcThree.h
@@ -0,0 +1,13 @@
+//
+//  vcThree.h
+//  SJGitUser
+//
+//  Created by weibiao li on 2018/4/9.
+//  Copyright © 2018年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface vcThree : UIViewController
+
+@end
---
+++ SJGitUser/vcThree.m
@@ -0,0 +1,37 @@
+//
+//  vcThree.m
+//  SJGitUser
+//
+//  Created by weibiao li on 2018/4/9.
+//  Copyright © 2018年 SJ. All rights reserved.
+//
+
+#import "vcThree.h"
+
+@interface vcThree ()
+
+@end
+
+@implementation vcThree
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
